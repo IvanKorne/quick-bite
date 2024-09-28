aca8dce307ee0e02b51edc587a4b14079f416b4f
Ivan Korneychuk <ikorneyc@uwaterloo.ca>
2024-09-28 16:34:50 -0400

    Connected to mongo db and imported shadcn button

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import { useState } from "react";
+import { Button } from "./components/ui/button";
 
 const App = () => {
-  return <div className="font-bold">App</div>;
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Button onClick={() => setCount((prev) => prev + 1)}>Click Me</Button>
+    </>
+  );
 };
 
 export default App;
