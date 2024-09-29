45c744c98318e20f829adc87ed3e928bef04ff49
Ivan Korneychuk <ikorneyc@uwaterloo.ca>
2024-09-28 21:23:49 -0400

    Created header for mobile and web

--- client/src/layouts/layout.tsx
+++ client/src/layouts/layout.tsx
@@ -5,6 +5,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
+
       <div className="container flex-1 py-10 mx-auto">{children}</div>
     </div>
   );
