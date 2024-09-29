90a577b0bf97d6e55b2030ea4fdf3d87e8366414
Ivan Korneychuk <ikorneyc@uwaterloo.ca>
2024-09-28 21:59:09 -0400

    Finished landing page

--- client/src/layouts/layout.tsx
+++ client/src/layouts/layout.tsx
@@ -1,12 +1,15 @@
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import Hero from "@/components/Hero";
 import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-
+      <Hero />
       <div className="container flex-1 py-10 mx-auto">{children}</div>
+      <Footer />
     </div>
   );
 };
